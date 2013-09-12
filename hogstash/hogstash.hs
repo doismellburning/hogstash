86babc18ac16a79e42881b8b6ea635ea2baa9af9
Kristian Glass <git@doismellburning.co.uk>
2013-09-12 14:31:35 +0100

    Lets not spin while creating threads
    http://stackoverflow.com/a/18765744/928098
    
    TL;DR I used `forever` to stop `main` from "finishing". That meant
    creating an unbounded amount of in/out threads, which didn't go so well

--- hogstash/hogstash.hs
+++ hogstash/hogstash.hs
@@ -6,9 +6,10 @@ import Control.Concurrent
 import Control.Concurrent.BoundedChan
 import Control.Monad
 
-main = forever $ do
+main = do
            channel <- newBoundedChan 10
            forkIO $ do
                connection <- tmpHaxx
                forever $ getEvent connection "logstash:beaver" channel
            forkIO $ forever $ stdout channel
+           forever $ threadDelay 1000 -- Block forever
