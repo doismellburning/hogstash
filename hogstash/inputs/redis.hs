013b8e7a19c0518bdb6d46b4f7c2b5c5a4e12c46
Kristian Glass <git@doismellburning.co.uk>
2013-09-12 01:40:44 +0100

    Skeleton Redis input

---
+++ hogstash/inputs/redis.hs
@@ -0,0 +1,34 @@
+import Database.Redis
+
+import Control.Concurrent
+import Control.Concurrent.BoundedChan as BC
+import qualified Data.ByteString.Char8 as BSC
+
+data Event = Event
+
+eventFromByteString :: BSC.ByteString -> Event
+eventFromByteString = undefined
+
+listListen key = blpop [key] 0
+
+getEvent :: ConnectInfo -> String -> BoundedChan Event -> IO ()
+
+getEvent a b = getEvent' a (BSC.pack b)
+
+getEvent' ci key channel = do
+								fnar <- pullEvent ci key
+								case fnar of
+									Just e -> BC.writeChan channel e
+									Nothing -> return ()
+
+pullEvent :: ConnectInfo -> BSC.ByteString -> IO (Maybe Event)
+pullEvent connect_info key = do
+									conn <- connect defaultConnectInfo
+									event_data <- runRedis conn $ listListen key
+									return (case event_data of
+										Left a -> Nothing
+										Right a -> extractEvent a)
+
+extractEvent :: Maybe (a, BSC.ByteString) -> Maybe Event
+extractEvent = fmap (eventFromByteString . snd)
+
